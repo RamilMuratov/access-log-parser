2b85a2929dfd84279d8e025358c0746e6d64d409
RamilMuratov <rmuratov@inno.tech>
2024-04-16 13:19:50 +0500

    Реализовано сравнение объектов через equals и вычитка доли яндекса и гугла в лог файле

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -22,33 +23,50 @@ public class Main {
             {
                 System.out.println("Файл не существует или введен неверный путь");
             }
-            try (
-                    FileReader fileReader = new FileReader(path)) {
-                BufferedReader reader =
-                        new BufferedReader(fileReader);
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
                 String line;
                 int sch = 0; // Счетчик: общее количество строк в файле
-                int min = reader.readLine().length(); // Самая короткая строка в файле
+                int min = 1024; // Самая короткая строка в файле
                 int max = 0; // Самая длинная строка в файле
+                double yandex = 0; //Доля Яндекс
+                double google = 0; //Доля Гугл
                 while ((line = reader.readLine()) != null) {
 
-                    if (line.length() > max){
+                    if (line.length() > max) {
                         max = line.length();
                     }
 
-                    if (max>=1024) throw new RuntimeException("Строка имеет длину 1024 символа или более");
+                    if (max >= 1024) throw new RuntimeException("Строка имеет длину 1024 символа или более");
 
-                    if (line.length() < min){
+                    if (line.length() < min) {
                         min = line.length();
                     }
-                    sch++;
-                }
-                System.out.println("Количество строк в файле: " + sch);
-                System.out.println("Самая короткая строка в файле равна "+ min +" символам");
-                System.out.println("Самая длинная строка в файле равна "+ max +" символам");
-                //System.out.println(offer);
+                    String[] parts1 = line.split(";");
+                    if (parts1.length >= 2) {
+                        String fragment = parts1[1];
+                        fragment = fragment.replaceAll(" ", "");
+                        String[] parts2 = fragment.split("/");
+                        if (parts2.length >= 2) {
+                            String fragment_rs = parts2[0];
+                            if (Objects.equals(fragment_rs, "YandexBot")) {
+                                yandex++;
+                            }
+                            if (Objects.equals(fragment_rs, "Googlebot")) {
+                                google++;
+                            }
+                        }
+                    }
+                        sch++;
+                    }
+                    System.out.println("Количество строк в файле: " + sch);
+                    System.out.println("Яндекс: "+ (yandex/sch));
+                    System.out.println("Гугл: "+ (google/sch));
             }
-            catch (Exception ex){ex.printStackTrace();}
+            catch (Exception ex){
+                    System.out.println(ex.getMessage());
+            }
         }
     }
 }
